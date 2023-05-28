09766ad3139d33a5be6592d2dd5b31c1fd318b18
yamesjamess <jameskasetarapanya15@gmail.com>
2023-05-28 14:43:25 +0000

    Create main function to display to main menu to the user

--- run.py
+++ run.py
@@ -20,4 +20,35 @@ patterns = SHEET.worksheet('patterns')
 
 data = patterns.get_all_values()
 
-print(data)
+def main():
+    """
+    Display the main menu to the user until they select an option or exit
+    """
+
+    while True:
+        print('Welcome to Yarn Genie! Your Magical Database for '
+            'Crochet Patterns, Yarns and Hooks\n')
+        print('1. View your patterns pieces.')
+        print('2. View your yarn stash.')
+        print('3. View your hook hoards.')
+        print('4. Calculate what you can make!')
+        print('5. Exit')
+
+        user_input = input('\nPlease select an option by entering a number from 1 - 5\n')
+
+        if user_input == '1':
+            print('call function show_patterns')
+        elif user_input == '2':
+            print('call function show_yarns')
+        elif user_input == '3':
+            print('call function show_hooks')
+        elif user_input == '4':
+            print('call function calculate')
+        elif user_input == '5':
+            print('Goodbye and stitch you later!')
+            break
+        else:
+            print('Invalid option, please eneter a number from 1 - 5\n')
+            input('Press Enter to continue...\n')
+
+main()
