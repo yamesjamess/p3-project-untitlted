aee9103fbdfb974bdad307cec0a232e55ca3f7ad
yamesjamess <jameskasetarapanya15@gmail.com>
2023-06-06 22:24:10 +0000

    Reformat the code to fix validator issue

--- run.py
+++ run.py
@@ -22,6 +22,7 @@ def clear():
     """
     print('\033c')
 
+
 # Worksheet Manipulation Functions
 def show_worksheet(worksheet):
     """
@@ -43,8 +44,9 @@ def show_worksheet(worksheet):
             row.insert(0, '')
         else:
             row.insert(0, index)
-    #turn list into table in the termial
+    # turn list into table in the termial
     print(tabulate(stash))
+
 
 def add_to_worksheet(data, worksheet):
     """
@@ -76,7 +78,7 @@ def remove_item(worksheet):
     Remove a row the user has selected from the relevant worksheet.
 
     Args:
-        worksheet: string: the name of the worksheet 
+        worksheet: string: the name of the worksheet
     """
     print(f'\nWhich item do you want to from your {worksheet} list?\n')
     worksheet_to_remove = SHEET.worksheet(worksheet)
@@ -89,7 +91,7 @@ def remove_item(worksheet):
             break
         elif user_input.isdigit() and user_input != '0':
             index = int(user_input)
-            index +=1
+            index += 1
             worksheet_to_remove.delete_rows(index)
             print(f'\nItem {user_input} has been removed!')
             input('\nPress Enter to continue...\n')
@@ -111,19 +113,22 @@ def get_user_data(type, num):
         type: string: the name of category of data user want to input
         num: int: display the length of csv user need to input
     Returns:
-        list: result of user csv input or blank if the user chose to exit program
+        list: result of user csv input or blank if the user chose to
+        exit program
     """
     while True:
         if type == 'pattern' or type == 'yarn':
             print(f'\nPlease enter your {type} information.')
-            print(f'\nInformation should be {num} categories, separate by commas.')
+            print(f'\nInformation should be {num} categories, '
+                  'separate by commas.')
 
         if type == 'pattern':
-            print(f'\n{type.capitalize()} Name, Yarn Weight, Yarn Length (m), Hook Size')
+            print(f'\n{type.capitalize()} Name, Yarn Weight, '
+                  'Yarn Length (m), Hook Size')
             print('Example: Kids Gloves, Double Knit, 400, 3.00\n')
 
             pattern_data = input(f'Enter your {type} information here '
-                                'or enter "x" to return to sub-menu:\n')
+                                 'or enter "x" to return to sub-menu:\n')
             if pattern_data.upper() == 'X':
                 print('Returning to sub-menu\n')
                 show_worksheet('patterns')
@@ -136,12 +141,12 @@ def get_user_data(type, num):
 
         elif type == 'yarn':
             print(f'\n{type.capitalize()} Name, Material, '
-                f'{type.capitalize()} Weight, {type.capitalize()} '
-                'Length, Colour, Quantity')
+                  f'{type.capitalize()} Weight, {type.capitalize()} '
+                  'Length, Colour, Quantity')
             print('Example: Rico, Cotton, Double Knit, 200, Teal, 1\n')
 
             yarn_data = input(f'Enter your {type} information here '
-                                'or enter "x" to return to sub-menu:\n')
+                              'or enter "x" to return to sub-menu:\n')
             if yarn_data.upper() == 'X':
                 print('Returning to sub-menu\n')
                 show_worksheet('yarns')
@@ -158,9 +163,9 @@ def get_user_data(type, num):
             print(f"\nNote: Only add hooks that you've owned!")
             print(f'\n{type.capitalize()} Size')
             print('Example: 6.00\n')
-            
+
             hook_data = input(f'Enter your {type} information here '
-                                'or enter "x" to return to sub-menu:\n')
+                              'or enter "x" to return to sub-menu:\n')
 
             if hook_data.upper() == 'X':
                 print('Returning to sub-menu\n')
@@ -183,17 +188,17 @@ def validate_pattern(values):
 
     Args:
         values: list: list of csv from get_user_data
-    
+
     Returns:
         boolean: result of validation
-    
+
     Raises:
         ValueError, if value is not equal to 4
     """
     try:
         if len(values) != 4:
             raise ValueError(
-                f'4 values required, you provided {len(values)}'    
+                f'4 values required, you provided {len(values)}'
             )
     except ValueError as e:
         print(f'Invalid data: {e}, please try again.\n')
@@ -208,19 +213,18 @@ def validate_yarn(values):
 
     Args:
         values: list: list of csv from get_user_data
-    
+
     Returns:
         boolean: result of validation
-    
+
     Raises:
         ValueError, if value is not equal to 6
     """
     try:
         if len(values) != 6:
             raise ValueError(
-                f'6 values required, you provided {len(values)}'    
-            )
-        
+                f'6 values required, you provided {len(values)}')
+
             # convert data at 3rd and 5th indices into intergers
             indices = [3, 5]
             for index in indices:
@@ -238,18 +242,17 @@ def validate_hook(values):
 
     Args:
         values: list: list of csv from get_user_data
-    
+
     Returns:
         boolean: result of validation
-    
+
     Raises:
         ValueError, if value is not equal to 1
     """
     try:
         if len(values) != 1:
             raise ValueError(
-                f'1 values required, you provided {len(values)}'    
-            )
+                f'1 values required, you provided {len(values)}')
     except ValueError as e:
         print(f'Invalid data: {e}, please try again.\n')
         return False
@@ -260,13 +263,13 @@ def validate_hook(values):
 # Calculation functions
 def calculate(user_input):
     """
-    Takes user input selection of pattern, then compare it against yarn and 
+    Takes user input selection of pattern, then compare it against yarn and
     hook stock to see if the user meet the requirement to make that pattern
     or not.
 
     Args:
         user_input: list: list of values from selected row on worksheet
-    
+
     Riases:
         ValueError: if input is not an integer
     """
@@ -280,58 +283,57 @@ def calculate(user_input):
 
     print('Calculating...\n')
 
-    #check if the user has the hook required for the project
+    # check if the user has the hook required for the project
     if user_input[3] in hooks_size:
         print(f'You have {user_input[3]}mm hook to make this project!\n')
     else:
-        print(f'This pattern requires a {user_input[3]}mm hook, ' 
-            'you do not have the hook. Go buy some hook!\n')
+        print(f'This pattern requires a {user_input[3]}mm hook, '
+              'you do not have the hook. Go buy some hook!\n')
 
-    #checks if there are more than 1 yarn that matches the same weight
-    #the user has selected
+    # checks if there are more than 1 yarn that matches the same weight
+    # the user has selected
     matched_lists = [lst for lst in yarns_data if lst[2] == user_input[1]]
-    #if there are more than 1 matches
+    # if there are more than 1 matches
     if len(matched_lists) > 1:
-        print(f'You have more than 1 of {user_input[1]} yarn, ' 
-            'please select a yarn you want to use\n')
+        print(f'You have more than 1 of {user_input[1]} yarn, '
+              'please select a yarn you want to use\n')
         for i, lst in enumerate(matched_lists):
             print(f"Yarn {i+1}: {lst}")
 
-        #asking the user to select which yarn they want to use 
-        #from the list above
+        # asking the user to select which yarn they want to use
+        # from the list above
         while True:
             try:
-                selected_yarn = int(input('\nEnter the yarn number you want to 
-                    'use (1, 2, etc.): \n').strip())
+                selected_yarn = int(input('\nEnter the yarn number you want '
+                                          'to use (1, 2, etc.): \n').strip())
                 selected_yarn -= 1
 
-                #print out the user's selection
+                # print out the user's selection
                 if selected_yarn >= 0 and selected_yarn < len(matched_lists):
                     selected_yarn_data = matched_lists[selected_yarn]
-                    print(f"\nYou've selected a {selected_yarn_data[1]} " 
-                        f"{selected_yarn_data[2]} yarn by " 
-                        f"{selected_yarn_data[0]}'s in "
-                        f"{selected_yarn_data[4]} colourway\n"
-                        )
-                    total_yarn_length = (int(selected_yarn_data[3]) * 
-                        int(selected_yarn_data[5]))
-                    #when user have more yarn than pattern requires
+                    print(f"\nYou've selected a {selected_yarn_data[1]} "
+                          f"{selected_yarn_data[2]} yarn by "
+                          f"{selected_yarn_data[0]}'s in "
+                          f"{selected_yarn_data[4]} colourway\n")
+                    total_yarn_length = (int(selected_yarn_data[3]) *
+                                         int(selected_yarn_data[5]))
+                    # when user have more yarn than pattern requires
                     if total_yarn_length > int(user_input[2]):
                         print('Congratulations! You have enough yarn to make '
-                            'this project!\n')
+                              'this project!\n')
                         input('Press Enter to go back to main menu...\n')
                         break
-                    #when user does not have enough yarn legnth
+                    # when user does not have enough yarn legnth
                     elif total_yarn_length < int(user_input[2]):
                         remaining_yarn = int(user_input[2]) - total_yarn_length
-                        additional_ball = ((remaining_yarn + 
-                            int(selected_yarn_data[3]) - 1) 
-                            // int(selected_yarn_data[3]))
-
+                        additional_ball = ((remaining_yarn +
+                                            int(selected_yarn_data[3]) - 1)
+                                           // int(selected_yarn_data[3]))
                         print(f'You have {selected_yarn_data[5]} ball(s) of '
-                            f'{selected_yarn_data[3]}m yarn. You would need '
-                            f'{additional_ball} more ball(s) ' 
-                            'more to make this project. Go and buy some more!\n')
+                              f'{selected_yarn_data[3]}m yarn. You would need '
+                              f'{additional_ball} more ball(s) '
+                              'more to make this project. '
+                              'Go and buy some more!\n')
                         input('Press Enter to go back to main menu...\n')
                         break
                 else:
@@ -339,41 +341,41 @@ def calculate(user_input):
             except ValueError:
                 print("Invalid input. Please enter a valid number.")
 
-
     # if there are only 1 match
     elif len(matched_lists) <= 1 and len(matched_lists) != 0:
-        #matched_lists is a nested list
-        #this turns it into a flat list for ease of computation
-        yarn_data=[]
+        # matched_lists is a nested list
+        # this turns it into a flat list for ease of computation
+        yarn_data = []
         for sublist in matched_lists:
             yarn_data.extend(sublist)
 
         total_yarn_length = int(yarn_data[3]) * int(yarn_data[5])
-        #when user have more yarn than pattern requires
+        # when user have more yarn than pattern requires
         if total_yarn_length > int(user_input[2]):
             print('Congratulations! You have enough yarn to make '
-                'this project!\n')
+                  'this project!\n')
             print(f"The yarn you will be using is {yarn_data[0]}'s "
-                f"{yarn_data[2]} {yarn_data[1]} yarn in "
-                f"{yarn_data[4]} colourway.\n")
+                  f"{yarn_data[2]} {yarn_data[1]} yarn in "
+                  f"{yarn_data[4]} colourway.\n")
             input('Press Enter to go back to main menu...\n')
-            return #recode to return to menu
-        #when user does not have enough yarn legnth
+            return
+        # when user does not have enough yarn legnth
         elif total_yarn_length < int(user_input[2]):
             remaining_yarn = int(user_input[2]) - total_yarn_length
-            additional_ball = ((remaining_yarn + int(yarn_data[3]) - 1) // 
-                int(yarn_data[3]))
+            additional_ball = ((remaining_yarn + int(yarn_data[3]) - 1) //
+                               int(yarn_data[3]))
 
             print(f'You have {yarn_data[5]} ball(s) of '
-                f'{yarn_data[3]}m yarn. You would need '
-                f'{additional_ball} more ball(s) ' 
-                'more to make this project. Go and buy some more yarns!\n')
+                  f'{yarn_data[3]}m yarn. You would need '
+                  f'{additional_ball} more ball(s) '
+                  'more to make this project. Go and buy some more yarns!\n')
             input('Press Enter to go back to main menu...\n')
-            return #recode to return to menu
+            return
     else:
-        print(f"To make this pattern, you'd need {user_input[1]} weight yarn.\n"
-            "You don't have any of them in your stash.\n"
-            "You should go and buy some yarns!\n")
+        print(f"To make this pattern, you'd need {user_input[1]}"
+              " weight yarn.\n"
+              "You don't have any of them in your stash.\n"
+              "You should go and buy some yarns!\n")
         input('Press Enter to go back to main menu...\n')
         return
 
@@ -390,16 +392,16 @@ def calc_menu():
     patterns_list = pattern.get_all_values()
     patterns_list.pop(0)
 
-    selected_row = input('\nPlease enter a number to select a pattern or enter '
-                        '"x" to return to the main menu\n').strip()
+    selected_row = input('\nPlease enter a number to select a pattern or '
+                         'enter "x" to return to the main menu\n').strip()
 
     while True:
         if selected_row.upper() == 'X':
             print('\nReturning to the main menu')
             input('\nPress Enter to continue...\n')
             break
-        elif selected_row.isdigit() and (0 < int(selected_row) <= 
-            len(patterns_list)):
+        elif selected_row.isdigit() and (0 < int(selected_row) <=
+                                         len(patterns_list)):
             row = int(selected_row)
             row += 1
             data = pattern.row_values(row)
@@ -412,14 +414,14 @@ def calc_menu():
         else:
             print('\nInvalid option, please try again...')
             selected_row = input('Enter a number or enter "x"'
-                ' to cancel...\n').strip()
+                                 ' to cancel...\n').strip()
 
 
 def sub_menu(str, worksheet, add_func, remove_func):
     """
     Display add, remove, and back sub menu until the user select a valid option
     then calls the requested funcion
-    
+
     Args:
         str: string: title of the submenu (patterns/yarns/hooks)
         worksheet: string: name of the worksheet on the spreadsheet
@@ -440,7 +442,7 @@ def sub_menu(str, worksheet, add_func, remove_func):
         print(f'3. Go back to main menu')
 
         user_input = input('\nPlease select an option by '
-            'entering a number from 1, 2, or 3\n').strip()
+                           'entering a number from 1, 2, or 3\n').strip()
 
         if user_input == '1':
             if str == 'pattern':
@@ -472,7 +474,8 @@ def art():
     print("    /  (  .-.    ).--..  .-.     /  (_;    .-..  .-.    `-' .-.  ")
     print("   (    )(  |   /      )/   )   /        ./.-'_)/   )  /  ./.-'_ ")
     print(" .  `..'  `-'-'/      '/   (   (     --;-(__.''/   (_.(__.(__.'  ")
-    print("(__.-'                      `-  `.___.'             `-           \n")
+    print("(__.-'                      `-  `.___.'             `-           "
+          "\n")
 
 
 def main_menu():
@@ -484,7 +487,7 @@ def main_menu():
         clear()
         art()
         print('Welcome to Yarn Genie! A Magical Database for '
-            'Crochet Patterns, Yarns and Hooks!\n')
+              'Crochet Patterns, Yarns and Hooks!\n')
         print('1. View your patterns pieces.')
         print('2. View your yarn stash.')
         print('3. View your hook hoards.')
@@ -492,10 +495,10 @@ def main_menu():
         print('5. Exit')
 
         user_input = input('\nPlease select an option by entering a '
-            'number from 1 - 5\n').strip()
+                           'number from 1 - 5\n').strip()
 
         if user_input == '1':
-            sub_menu('pattern','patterns', get_user_data, remove_item)
+            sub_menu('pattern', 'patterns', get_user_data, remove_item)
         elif user_input == '2':
             sub_menu('yarn', 'yarns', get_user_data, remove_item)
         elif user_input == '3':
